3936357fe25ebf77c950bfee106482c32b9ceb20
Thomas Eastman <tjeastman@gmail.com>
2014-11-24 00:38:16 -0800

    fix function signature

--- usart.h
+++ usart.h
@@ -1,7 +1,6 @@
 #ifndef USART_H
 #define USART_H
 void usart_init(void);
-void usart_putchar(char c);
-int usart_putchar_printf(char c, FILE *stream);
+int usart_putchar(char c, FILE *stream);
 extern FILE usart_output;
 #endif

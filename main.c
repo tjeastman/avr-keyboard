bb94c11c97dd45d32e73b7ecf2ea42ce8543abb7
Thomas Eastman <tjeastman@gmail.com>
2014-11-23 16:47:27 -0800

    use switch statement to process a scan code

--- main.c
+++ main.c
@@ -13,9 +13,11 @@ ISR(INT0_vect) {
   static kstate keyboard_state = KEYBOARD_START;
   static int keyboard_bits_read = 0;
   static unsigned char keyboard_value = 0;
-  if (keyboard_state == KEYBOARD_START) {
+  switch (keyboard_state) {
+  case KEYBOARD_START:
     keyboard_state = KEYBOARD_SCAN;
-  } else if (keyboard_state == KEYBOARD_SCAN) {
+    break;
+  case KEYBOARD_SCAN:
     keyboard_value = keyboard_value >> 1;
     if (PIND & (1<<PD3))
       keyboard_value |= 0x80;
@@ -24,13 +26,18 @@ ISR(INT0_vect) {
       keyboard_bits_read = 0;
       keyboard_state = KEYBOARD_PARITY;
     }
-  } else if (keyboard_state == KEYBOARD_PARITY) {
+    break;
+  case KEYBOARD_PARITY:
     keyboard_state = KEYBOARD_END;
-  } else {
+    break;
+  case KEYBOARD_END:
     keyboard_state = KEYBOARD_START;
     keyboard_buffer[keyboard_buffer_pos] = keyboard_value;
     keyboard_value = 0;
     keyboard_buffer_pos += 1;
+    break;
+  default:
+    break; // should not get here
   }
 }
 

18f597a70266dc42027c020bda90008c79d14e25
Thomas Eastman <tjeastman@gmail.com>
2014-11-23 21:00:07 -0800

    use a data structure to record keyboard scan codes

--- keyboard.c
+++ keyboard.c
@@ -31,29 +31,32 @@ void reset_buffer(void) {
 
 void keyboard_interrupt(void) {
   static kstate keyboard_state = KEYBOARD_START;
-  static int keyboard_bits_read = 0;
-  static unsigned char keyboard_value = 0;
+  static scan_code c;
+
   switch (keyboard_state) {
   case KEYBOARD_START:
     keyboard_state = KEYBOARD_SCAN;
+    c.value = 0;
+    c.nbits = 0;
+    c.parity = 0;
     break;
   case KEYBOARD_SCAN:
-    keyboard_value = keyboard_value >> 1;
+    c.value = c.value >> 1;
     if (PIND & (1<<PD3))
-      keyboard_value |= 0x80;
-    keyboard_bits_read += 1;
-    if (keyboard_bits_read == 8) {
-      keyboard_bits_read = 0;
+      c.value |= 0x80;
+    c.nbits += 1;
+    if (c.nbits == 8) {
+      c.nbits = 0;
       keyboard_state = KEYBOARD_PARITY;
     }
     break;
   case KEYBOARD_PARITY:
     keyboard_state = KEYBOARD_END;
+    c.parity = PIND & (1<<PD3) ? 1 : 0;
     break;
   case KEYBOARD_END:
     keyboard_state = KEYBOARD_START;
-    keyboard_buffer[keyboard_buffer_pos] = keyboard_value;
-    keyboard_value = 0;
+    keyboard_buffer[keyboard_buffer_pos] = c.value;
     keyboard_buffer_pos += 1;
     break;
   default:
--- keyboard.h
+++ keyboard.h
@@ -8,6 +8,12 @@ typedef enum {
   KEYBOARD_END = 3,
 } kstate;
 
+typedef struct {
+  uint8_t value;
+  uint8_t nbits; // number of bits received from the device
+  uint8_t parity;
+} scan_code;
+
 void keyboard_init(void);
 int get_buffer_size(void);
 unsigned char get_buffer_item(int);

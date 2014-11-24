a49d6d381bdad25b850cd0d14e874fc736c917b0
Thomas Eastman <tjeastman@gmail.com>
2014-11-24 00:16:51 -0800

    simplify keyboard interrupt handler

--- keyboard.c
+++ keyboard.c
@@ -27,31 +27,51 @@ void keyboard_init(void) {
   EICRA |= 1<<ISC01 | 0<<ISC00;
 }
 
+inline void scan_code_reset(struct scan_code *code)
+{
+  code->value = 0;
+  code->parity = 0;
+  code->nbits = 0;
+}
+
+inline enum scan_state scan_state_transition(enum scan_state state, struct scan_code code)
+{
+  if (state == SCAN_START)
+    return SCAN_DATA;
+  else if (state == SCAN_DATA && code.nbits < 8)
+    return SCAN_DATA;
+  else if (state == SCAN_DATA && code.nbits == 8)
+    return SCAN_PARITY;
+  else if (state == SCAN_PARITY)
+    return SCAN_END;
+  else if (state == SCAN_END)
+    return SCAN_START;
+
+  return state;
+}
+
 void keyboard_interrupt(void) {
   static enum scan_state state = SCAN_START;
   static struct scan_code c;
 
   switch (state) {
   case SCAN_START:
-    state = SCAN_DATA;
-    c.value = 0;
-    c.nbits = 0;
-    c.parity = 0;
+    state = scan_state_transition(state, c);
+    scan_code_reset(&c);
     break;
   case SCAN_DATA:
     c.value = c.value >> 1;
     if (PIND & (1<<PD3))
       c.value |= 0x80;
     c.nbits += 1;
-    if (c.nbits == 8)
-      state = SCAN_PARITY;
+    state = scan_state_transition(state, c);
     break;
   case SCAN_PARITY:
-    state = SCAN_END;
+    state = scan_state_transition(state, c);
     c.parity = PIND & (1<<PD3) ? 1 : 0;
     break;
   case SCAN_END:
-    state = SCAN_START;
+    state = scan_state_transition(state, c);
     // put the scan code into the buffer
     buffer_head->state = SCAN_END;
     buffer_head->code.value = c.value;

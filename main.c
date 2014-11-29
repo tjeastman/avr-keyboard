40e719c51c4753c3cc85a83b9e5536e936240a0d
Thomas Eastman <tjeastman@gmail.com>
2014-11-28 18:27:51 -0800

    move shift key labels into key data structure

--- main.c
+++ main.c
@@ -33,155 +33,103 @@ void keyboard_state_transition(struct keyboard_state *state, struct scan_code *c
 }
 
 struct key {
-  char *label;
   uint8_t value;
+  char *label;
+  char *label_shift;
 };
 
 struct key_page {
   struct key *keys;
-  struct key *shift_keys;
   unsigned int size;
-  unsigned int shift_size;
 };
 
 struct key keys[] =
   {
-    {"[F9]", 0x01},
-    {"[F5]", 0x03},
-    {"[F3]", 0x04},
-    {"[F1]", 0x05},
-    {"[F2]", 0x06},
-    {"[F12]", 0x07},
-    {"[F10]", 0x09},
-    {"[F8]", 0x0A},
-    {"[F6]", 0x0B},
-    {"[F4]", 0x0C},
-    {"\t", 0x0D},
-    {"`", 0x0E},
-    {"q", 0x15},
-    {"1", 0x16},
-    {"z", 0x1A},
-    {"s", 0x1B},
-    {"a", 0x1C},
-    {"w", 0x1D},
-    {"2", 0x1E},
-    {"c", 0x21},
-    {"x", 0x22},
-    {"d", 0x23},
-    {"e", 0x24},
-    {"4", 0x25},
-    {"3", 0x26},
-    {" ", 0x29},
-    {"v", 0x2A},
-    {"f", 0x2B},
-    {"t", 0x2C},
-    {"r", 0x2D},
-    {"5", 0x2E},
-    {"n", 0x31},
-    {"b", 0x32},
-    {"h", 0x33},
-    {"g", 0x34},
-    {"y", 0x35},
-    {"6", 0x36},
-    {"m", 0x3A},
-    {"j", 0x3B},
-    {"u", 0x3C},
-    {"7", 0x3D},
-    {"8", 0x3E},
-    {",", 0x41},
-    {"k", 0x42},
-    {"i", 0x43},
-    {"o", 0x44},
-    {"0", 0x45},
-    {"9", 0x46},
-    {".", 0x49},
-    {"/", 0x4A},
-    {"l", 0x4B},
-    {";", 0x4C},
-    {"p", 0x4D},
-    {"-", 0x4E},
-    {"'", 0x52},
-    {"[", 0x54},
-    {"=", 0x55},
-    {"[CAPS]", 0x58},
-    {"\r\n", 0x5A},
-    {"]", 0x5B},
-    {"\\", 0x5D},
-    {"[BACKSPACE]", 0x66},
-    {"[ESC]", 0x76},
-    {"[NUM]", 0x77},
-    {"[F11]", 0x78},
-    {"[SCROLL]", 0x7E},
-    {"[F7]", 0x83}
-  };
-
-struct key shift_keys[] =
-  {
-    {"~", 0x0E},
-    {"Q", 0x15},
-    {"!", 0x16},
-    {"Z", 0x1A},
-    {"S", 0x1B},
-    {"A", 0x1C},
-    {"W", 0x1D},
-    {"@", 0x1E},
-    {"C", 0x21},
-    {"X", 0x22},
-    {"D", 0x23},
-    {"E", 0x24},
-    {"$", 0x25},
-    {"#", 0x26},
-    {"V", 0x2A},
-    {"F", 0x2B},
-    {"T", 0x2C},
-    {"R", 0x2D},
-    {"%", 0x2E},
-    {"N", 0x31},
-    {"B", 0x32},
-    {"H", 0x33},
-    {"G", 0x34},
-    {"Y", 0x35},
-    {"^", 0x36},
-    {"M", 0x3A},
-    {"J", 0x3B},
-    {"U", 0x3C},
-    {"&", 0x3D},
-    {"*", 0x3E},
-    {"<", 0x41},
-    {"K", 0x42},
-    {"I", 0x43},
-    {"O", 0x44},
-    {")", 0x45},
-    {"(", 0x46},
-    {">", 0x49},
-    {"?", 0x4A},
-    {"L", 0x4B},
-    {":", 0x4C},
-    {"P", 0x4D},
-    {"_", 0x4E},
-    {"\"", 0x52},
-    {"{", 0x54},
-    {"+", 0x55},
-    {"}", 0x5B},
-    {"|", 0x5D},
+    {0x01, "[F9]", NULL},
+    {0x03, "[F5]", NULL},
+    {0x04, "[F3]", NULL},
+    {0x05, "[F1]", NULL},
+    {0x06, "[F2]", NULL},
+    {0x07, "[F12]", NULL},
+    {0x09, "[F10]", NULL},
+    {0x0A, "[F8]", NULL},
+    {0x0B, "[F6]", NULL},
+    {0x0C, "[F4]", NULL},
+    {0x0D, "\t", NULL},
+    {0x0E, "`", "~"},
+    {0x15, "q", "Q"},
+    {0x16, "1", "!"},
+    {0x1A, "z", "Z"},
+    {0x1B, "s", "S"},
+    {0x1C, "a", "A"},
+    {0x1D, "w", "W"},
+    {0x1E, "2", "@"},
+    {0x21, "c", "C"},
+    {0x22, "x", "X"},
+    {0x23, "d", "D"},
+    {0x24, "e", "E"},
+    {0x25, "4", "$"},
+    {0x26, "3", "#"},
+    {0x29, " ", NULL},
+    {0x2A, "v", "V"},
+    {0x2B, "f", "F"},
+    {0x2C, "t", "T"},
+    {0x2D, "r", "R"},
+    {0x2E, "5", "%"},
+    {0x31, "n", "N"},
+    {0x32, "b", "B"},
+    {0x33, "h", "H"},
+    {0x34, "g", "G"},
+    {0x35, "y", "Y"},
+    {0x36, "6", "^"},
+    {0x3A, "m", "M"},
+    {0x3B, "j", "J"},
+    {0x3C, "u", "U"},
+    {0x3D, "7", "&"},
+    {0x3E, "8", "*"},
+    {0x41, ",", "<"},
+    {0x42, "k", "K"},
+    {0x43, "i", "I"},
+    {0x44, "o", "O"},
+    {0x45, "0", ")"},
+    {0x46, "9", "("},
+    {0x49, ".", ">"},
+    {0x4A, "/", "?"},
+    {0x4B, "l", "L"},
+    {0x4C, ";", ":"},
+    {0x4D, "p", "P"},
+    {0x4E, "-", "_"},
+    {0x52, "'", "\""},
+    {0x54, "[", "{"},
+    {0x55, "=", "+"},
+    {0x58, "[CAPS]", NULL},
+    {0x5A, "\r\n", NULL},
+    {0x5B, "]", "}"},
+    {0x5D, "\\", "|"},
+    {0x66, "[BACKSPACE]", NULL},
+    {0x76, "[ESC]", NULL},
+    {0x77, "[NUM]", NULL},
+    {0x78, "[F11]", NULL},
+    {0x7E, "[SCROLL]", NULL},
+    {0x83, "[F7]", NULL}
   };
 
 struct key extended_keys[] =
   {
-    {"[END]", 0x69},
-    {"[LEFT]", 0x6B},
-    {"[HOME]", 0x6C},
-    {"[INS]", 0x70},
-    {"[DEL]", 0x71},
-    {"[DOWN]", 0x72},
-    {"[RIGHT]", 0x74},
-    {"[UP]", 0x75},
-    {"[PGDOWN]", 0x7A},
-    {"[PGUP]", 0x7D}
+    {0x69, "[END]", NULL},
+    {0x6B, "[LEFT]", NULL},
+    {0x6C, "[HOME]", NULL},
+    {0x70, "[INS]", NULL},
+    {0x71, "[DEL]", NULL},
+    {0x72, "[DOWN]", NULL},
+    {0x74, "[RIGHT]", NULL},
+    {0x75, "[UP]", NULL},
+    {0x7A, "[PGDOWN]", NULL},
+    {0x7D, "[PGUP]", NULL}
   };
 
-struct key_page default_key_page = { keys, shift_keys, 67, 47 };
-struct key_page extended_key_page = { extended_keys, NULL, 10, 0 };
+struct key_page default_key_page = { keys, 67 };
+struct key_page extended_key_page = { extended_keys, 10 };
 
 int compare_keys(const void *k1, const void *k2)
 {
@@ -190,27 +138,14 @@ int compare_keys(const void *k1, const void *k2)
   return key1->value - key2->value;
 }
 
-char *lookup_key(struct scan_code *code, struct key_page *current_keys)
+struct key *lookup_key(struct scan_code *code, struct key_page *current_keys)
 {
   struct key search_key;
   struct key *found_key;
   search_key.value = code->value;
   found_key = bsearch(&search_key, current_keys->keys, current_keys->size, sizeof(struct key), compare_keys);
   if (found_key) {
-    return found_key->label;
-  } else {
-    return NULL;
-  }
-}
-
-char *lookup_shift_key(struct scan_code *code, struct key_page *current_keys)
-{
-  struct key search_key;
-  struct key *found_key;
-  search_key.value = code->value;
-  found_key = bsearch(&search_key, current_keys->shift_keys, current_keys->shift_size, sizeof(struct key), compare_keys);
-  if (found_key) {
-    return found_key->label;
+    return found_key;
   } else {
     return NULL;
   }
@@ -220,6 +155,7 @@ char *decode(struct scan_code *code)
 {
   static struct keyboard_state state = { 0, 0 };
   static struct key_page *current_keys = &default_key_page;
+  struct key *found_key;
   char *label = NULL;
 
   keyboard_state_transition(&state, code);
@@ -235,12 +171,19 @@ char *decode(struct scan_code *code)
   } else if (is_release_code(code)) {
     state.release_mode = 1;
   } else if (keyboard_shift_pressed(&state)) {
-    label = lookup_shift_key(code, current_keys);
-    if (!label) {
-      label = lookup_key(code, current_keys);
+    found_key = lookup_key(code, current_keys);
+    if (found_key) {
+      if (found_key->label_shift) {
+        label = found_key->label_shift;
+      } else {
+        label = found_key->label;
+      }
     }
   } else {
-    label = lookup_key(code, current_keys);
+    found_key = lookup_key(code, current_keys);
+    if (found_key) {
+      label = found_key->label;
+    }
   }
 
   // switch back to the default "page" of scan codes

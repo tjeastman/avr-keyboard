7cd25706378129d08e5aa82c5d04cbbefd925873
Thomas Eastman <tjeastman@gmail.com>
2016-05-30 11:06:15 -0700

    update for ATmega32u4

--- usart.c
+++ usart.c
@@ -6,21 +6,23 @@
 
 void usart_init(void)
 {
-    UBRR0H = UBRRH_VALUE;
-    UBRR0L = UBRRL_VALUE;
+    UBRR1H = UBRRH_VALUE;
+    UBRR1L = UBRRL_VALUE;
 
 #if USE_2X
-    UCSR0A |= _BV(U2X0);
+    UCSR1A |= _BV(U2X1);
 #else
-    UCSR0A &= ~(_BV(U2X0));
+    UCSR1A &= ~(_BV(U2X1));
 #endif
 
-    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); // 8-bit data
-    UCSR0B = _BV(RXEN0) | _BV(TXEN0); // enable RX and TX
+    UCSR1C = _BV(UCSZ11) | _BV(UCSZ10); // 8-bit data
+    UCSR1B = _BV(RXEN1) | _BV(TXEN1); // enable RX and TX
 }
 
 void usart_putchar(char c)
 {
-  loop_until_bit_is_set(UCSR0A, UDRE0);
-  UDR0 = c;
+  loop_until_bit_is_set(UCSR1A, UDRE1);
+  UDR1 = c;
 }
+
+FILE usart_output = FDEV_SETUP_STREAM(usart_putchar, NULL, _FDEV_SETUP_WRITE);
